9832d0313ba7a67a435b64661d4f2da18883fd91
pm <p.mroz.zywiol@gmail.com>
2023-08-16 19:07:41 +0200

    misc: added multiple run configurations (prod and dev)

--- apps/wiki/src/app/pages/welcome/welcome.component.ts
+++ apps/wiki/src/app/pages/welcome/welcome.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {environment} from '../../../environments/environment';
 
 @Component({
   selector: 'space-settlements-saga-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.scss'],
 })
-export class WelcomeComponent {}
+export class WelcomeComponent {
+  env = environment.environmentType
+}
